99ff7edd0b161c3c4a6a4d50e01bcd8b34278072
Shuchakaborty <shubhajitichapur@gmail.com>
2024-08-17 09:16:32 +0530

    added new pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,8 @@ import App from "./App";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import CreateCard from "./pages/CreateCard";
-
+import Candidatelist from "./pages/Candidatelist";
+import Card from "./pages/Card";
 
 const routes = createBrowserRouter(
 
@@ -17,6 +18,8 @@ const routes = createBrowserRouter(
       <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
       <Route path="create/card" element={<CreateCard />} />
+      <Route path="candidatelist" element={<Candidatelist />} />
+      <Route path="card" element={<Card />} />
     </Route>
   )
 );
